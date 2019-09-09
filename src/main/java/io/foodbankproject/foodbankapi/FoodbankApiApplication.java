eca25f2b92fa26f2f431d339cf703431265b1d0b
Thomas Winterfield <twinterf@uncc.edu>
2019-09-09 16:24:53 -0400

    initial commit

---
+++ src/main/java/io/foodbankproject/foodbankapi/FoodbankApiApplication.java
@@ -0,0 +1,13 @@
+package io.foodbankproject.foodbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodbankApiApplication.class, args);
+	}
+
+}
