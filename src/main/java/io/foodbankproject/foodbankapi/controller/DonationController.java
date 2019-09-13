952e05700846be8bc363c57f58b2dd63e9c53fe3
ThomasW0221 <twinterf@uncc.edu>
2019-09-12 20:44:43 -0400

    Gets for DonationController

--- src/main/java/io/foodbankproject/foodbankapi/controller/DonationController.java
+++ src/main/java/io/foodbankproject/foodbankapi/controller/DonationController.java
@@ -1,8 +1,31 @@
 package io.foodbankproject.foodbankapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import io.foodbankproject.foodbankapi.entity.Donation;
+import io.foodbankproject.foodbankapi.repository.DonationRepository;
+
+@RestController("/donations")
 public class DonationController {
 
+	@Autowired
+	private DonationRepository donationRepository;
+	
+	@GetMapping("/getAll")
+	public Iterable<Donation> getAll() {
+		return donationRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Donation getOne(@PathVariable("id") Integer id) {
+		//TODO: Validate that we have an item present at that id
+		//Hint, look at methods available on donationRepository
+		
+		return donationRepository.findById(id).orElse(null);
+	}
 }
