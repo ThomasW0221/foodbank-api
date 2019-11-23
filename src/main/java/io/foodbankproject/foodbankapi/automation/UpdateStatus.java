0a5793884701b25b2747e61071cc2b5c904e9845
ThomasW0221 <twinterf@uncc.edu>
2019-11-23 13:18:26 -0500

    add cron and adjust tweet

--- src/main/java/io/foodbankproject/foodbankapi/automation/UpdateStatus.java
+++ src/main/java/io/foodbankproject/foodbankapi/automation/UpdateStatus.java
@@ -1,11 +1,15 @@
 package io.foodbankproject.foodbankapi.automation;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import io.foodbankproject.foodbankapi.entity.Donation;
+import io.foodbankproject.foodbankapi.service.FullDonationService;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -15,21 +19,43 @@ public class UpdateStatus{
 	
 	TwitterFactory ourTwitterFactory;
 	Twitter twitterInstance;
-	LocalDateTime ldt = LocalDateTime.now();
+	FullDonationService fullDonationService;
 	
 	@Autowired
-	public UpdateStatus(TwitterFactory twitterFactory) {
-		ourTwitterFactory = twitterFactory;
-		twitterInstance = twitterFactory.getInstance();
+	public UpdateStatus(TwitterFactory twitterFactory, FullDonationService fullDonationService) {
+		this.ourTwitterFactory = twitterFactory;
+		this.twitterInstance = twitterFactory.getInstance();
+		this.fullDonationService = fullDonationService;
 	}
 	
-	@Scheduled(fixedRate=5000)
+	@Scheduled(cron="0 18 * * 1-5")
 	public void testTweet() {
+//		try {
+//			twitterInstance.updateStatus(ldt.toString());
+//			ldt = ldt.plusDays(1);
+//		} catch (TwitterException e) {
+//			e.printStackTrace();
+//		}
+		
+		LocalDateTime today = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS);
+		String todayString = today.toString();
+		List<Donation> todaysDonations = getTodaysDonations(todayString);
+		int numberOfTodaysDonations = todaysDonations.size();
+		
+		String tweetText = String.format("On %s, we received %d donations. Stop by and donate today!",
+				todayString,
+				numberOfTodaysDonations);
+		
 		try {
-			twitterInstance.updateStatus(ldt.toString());
-			ldt = ldt.plusDays(1);
-		} catch (TwitterException e) {
-			e.printStackTrace();
+			twitterInstance.updateStatus(tweetText);
+		} catch (TwitterException twe) {
+			twe.printStackTrace();
 		}
 	}
+
+	private List<Donation> getTodaysDonations(String todaysDate) {
+		return fullDonationService.donationFindByFromDate(todaysDate);
+	}
+
+	
 }
